2dc3e24470dae4ef90148edc90954514b0982225
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-19 13:29:31 +0800

    first commit

---
+++ test_blogsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-2s2naj^x1z)@j_(6)@!q)yl2!*1e@+5i@*)sfswda&fe+qa3pg'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
